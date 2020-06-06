41d6e3f000a3f19da22298e086139a5a781c633c
NewWebSouth <romainlavabre98@gmail.com>
2020-06-06 02:57:56 +0200

    Stable 2.17.1

--- Component/DataManager/DataManager.php
+++ Component/DataManager/DataManager.php
@@ -576,7 +576,7 @@ class DataManager
                     );
                 }else{
                     $persistsManager->$method(
-                        $type
+                        $param[0]
                     );
                 }
 
