88f7f9ecd5ffef4e0071ca6dc808df8852c950df
NewWebSouth <romainlavabre98@gmail.com>
2020-08-05 03:57:07 +0200

    2.20.4

--- Http/HttpSession.php
+++ Http/HttpSession.php
@@ -7,7 +7,7 @@ use Nomess\Exception\NomessException;
 
 class HttpSession
 {
-
+    
     private const ID_MODULE_SECURITY = 'nomess_session_security';
     private const MODULE_INITIALIZE = 'initialized';
     private const MODULE_USER_AGENT = 'user_agent';
@@ -16,8 +16,8 @@ class HttpSession
     private const BIND_TICKET_IP = 'bind_ticket_ip';
     private const RECOVERY_CONFIG = 'recovery_config';
     private const SESSION_SET_LIFE_TIME = 'session_set_life_time';
-
-
+    
+    
     /**
      * Initialize an session
      *
@@ -25,22 +25,22 @@ class HttpSession
      */
     public function initSession(): void
     {
-
+        
         if (session_status() === 1) {
-
+            
             ini_set('session.gc_maxlifetime', getenv('NM_SESSION_LIFETIME'));
             session_start();
             if (!isset($_SESSION[self::ID_MODULE_SECURITY])) {
                 $_SESSION[self::ID_MODULE_SECURITY] = array();
             }
-
+            
             $this->securityInitialized();
-
+            
             if (isset($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_USER_AGENT])) {
                 $this->executedModule();
             }
-
-
+            
+            
         } else if (session_status() === 0) {
             throw new NomessException('Please active the session');
         }
@@ -55,7 +55,7 @@ class HttpSession
     {
         return isset($_SESSION[$index]);
     }
-
+    
     /**
      * Return reference of the entry associate to index
      * Null if doesn't exists
@@ -69,7 +69,7 @@ class HttpSession
             return $_SESSION[$index];
         }
     }
-
+    
     /**
      * Return the value assiciate to index, Null if doesn't exists
      *
@@ -84,8 +84,8 @@ class HttpSession
             return null;
         }
     }
-
-
+    
+    
     /**
      * Delete the entry associate to index variable
      *
@@ -100,7 +100,7 @@ class HttpSession
         
         return $this;
     }
-
+    
     /**
      * Add value
      *
@@ -114,9 +114,9 @@ class HttpSession
         if ($reset) {
             unset($_SESSION[$key]);
         }
-
+        
         if (\is_array($value)) {
-
+            
             if(!array_key_exists($key, $_SESSION) || !is_array($_SESSION[$key])){
                 $_SESSION[$key] = array();
             }
@@ -128,7 +128,7 @@ class HttpSession
         } else {
             $_SESSION[$key] = $value;
         }
-    
+        
         return $this;
     }
     
@@ -145,16 +145,16 @@ class HttpSession
         
         if (!array_key_exists(self::SESSION_SET_LIFE_TIME, $_SESSION[self::ID_MODULE_SECURITY]) || $force === true) {
             $content = $_SESSION;
-
+            
             session_destroy();
             ini_set('session.gc_maxlifetime', $time);
             session_set_cookie_params($time);
             session_start();
-
+            
             $_SESSION = $content;
-
+            
             $_SESSION[self::ID_MODULE_SECURITY][self::SESSION_SET_LIFE_TIME] = true;
-
+            
         }
         
         return $this;
@@ -167,20 +167,21 @@ class HttpSession
     public function kill(): self
     {
         $toolbar = $_SESSION['app']['toolbar'];
-        
         $_SESSION = array();
+        session_destroy();
+        session_start();
         $_SESSION['app']['toolbar'] = $toolbar;
         
         return $this;
     }
-
-
-
+    
+    
+    
     /*
      * Configuration des brique de sécurité
      */
-
-
+    
+    
     /**
      * Installation of security module, valid for lifetime of session
      *
@@ -198,44 +199,44 @@ class HttpSession
         if ($ticketSystem === true) {
             setcookie('dpr__', 'sdf846dsf68fs3k4f4rs53f8sddfre', time() + 60 * 20, '/');
         }
-
-
+        
+        
         $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET] = $ticketSystem;
         $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_USER_AGENT] = ($userAgentSystem === true) ? $_SERVER['HTTP_USER_AGENT'] : false;
         $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_IP] = ($ipSystem === true) ? $_SERVER['REMOTE_ADDR'] : null;
         $_SESSION[self::ID_MODULE_SECURITY][self::BIND_TICKET_IP] = $bindTicketIp;
         $_SESSION[self::ID_MODULE_SECURITY][self::RECOVERY_CONFIG] = $recoveryConfig;
-
+        
         if ($recoveryConfig !== null && count($recoveryConfig) !== 2) {
             throw new InvalidParamException('RecoveryConfig must contain exactly 2 parameters: $userAgentSystem and $ipSystem| $ticketSystem and $bindTicketIp will be initialized to false');
         }
         
         return $this;
     }
-
-
+    
+    
     /*
      * Executor
      */
-
-
+    
+    
     /**
      * Execute the modules
      */
     private function executedModule(): void
     {
         $success = true;
-
+        
         if ($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_USER_AGENT] !== false) {
             $success = $this->securityUserAgent();
         }
-
+        
         if ($success === true) {
             if (isset($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET]) && $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET] === true) {
                 $success = $this->securityTicket();
             }
         }
-
+        
         if ($success === true) {
             if ($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_IP] !== false) {
                 $success = $this->securityIpUser();
@@ -244,22 +245,22 @@ class HttpSession
                 }
             }
         }
-
-
+        
+        
         //r($success);
         if ($success === false) {
             session_regenerate_id(true);
             $_SESSION = array();
         }
     }
-
-
-
+    
+    
+    
     /*
      *  Module de sécurité de session
      */
-
-
+    
+    
     /**
      * Module Initialized
      *
@@ -272,13 +273,13 @@ class HttpSession
             $_SESSION[self::ID_MODULE_SECURITY] = array();
             $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_INITIALIZE] = 1;
         }
-
+        
         if(!isset($_SESSION['app']['_token'])){
             $_SESSION['app']['_token'] = md5(uniqid('_token::') . str_shuffle('ABCDEFGHIJ'));
         }
     }
-
-
+    
+    
     /**
      * Module useragent
      *
@@ -289,13 +290,13 @@ class HttpSession
         if ($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_USER_AGENT] === $_SERVER['HTTP_USER_AGENT']) {
             return true;
         } else {
-
+            
             return false;
         }
-
+        
     }
-
-
+    
+    
     /**
      * Module ticket
      *
@@ -304,10 +305,10 @@ class HttpSession
      */
     private function securityTicket(): bool
     {
-
+        
         if ($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_INITIALIZE] === 1) {
             $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_INITIALIZE]++;
-
+            
             if (!isset($_COOKIE['dpr__'])) {
                 $this->installSecurityModules(
                     $_SESSION[self::ID_MODULE_SECURITY][self::RECOVERY_CONFIG][0],
@@ -315,31 +316,31 @@ class HttpSession
                     $_SESSION[self::ID_MODULE_SECURITY][self::RECOVERY_CONFIG][1]
                 );
             }
-
+            
             $this->getTicket();
-
+            
             unset($_SESSION[self::ID_MODULE_SECURITY][self::RECOVERY_CONFIG]);
-
+            
             return true;
         } else if (isset($_COOKIE[self::MODULE_TICKET]) && isset($_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET])) {
-
+            
             if ($_COOKIE[self::MODULE_TICKET] === $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET]) {
-
+                
                 $this->getTicket();
-
+                
                 return true;
             } else {
                 return false;
             }
         } else {
             session_regenerate_id(true);
-
+            
             $this->getTicket();
             return false;
         }
     }
-
-
+    
+    
     /**
      * Create an ticket on demand
      */
@@ -347,12 +348,12 @@ class HttpSession
     {
         $ticket = session_id() . microtime() . rand(0, 9999999999);
         $ticket = md5($ticket);
-
+        
         setcookie(self::MODULE_TICKET, $ticket, time() + (60 * 20), '/');
         $_SESSION[self::ID_MODULE_SECURITY][self::MODULE_TICKET] = $ticket;
     }
-
-
+    
+    
     /**
      * Module IpUser
      *
