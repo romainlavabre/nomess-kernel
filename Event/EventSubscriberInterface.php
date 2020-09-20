bbbb419af3723a67c084ad8bdd2bc43c78b5e458
master <romainlavabre98@gmail.com>
2020-09-20 16:33:02 +0200

    [EventListener] OK

--- Event/EventSubscriberInterface.php
+++ Event/EventSubscriberInterface.php
@@ -7,7 +7,7 @@ namespace Nomess\Event;
 interface EventSubscriberInterface
 {
     
-    public function subscribe(): void;
+    public function subscribe( EventListenerInterface $eventListener ): void;
     
     
     public function notified( string $event, $value ): void;
